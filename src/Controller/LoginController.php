b377a71d020bcf0e35c99cbbff47512f38a5d7e1
Jelle Z. <public@jellezwie.rs>
2023-11-20 09:12:07 +0100

    Small alterations login controller (#13)

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,11 +7,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
