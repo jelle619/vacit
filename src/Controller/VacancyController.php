145fc5c5a87c9b44fd1133b6ad0eaa4aa4c5d934
Jelle Z. <public@jellezwie.rs>
2023-11-20 09:08:58 +0100

    Vacancy controller (#11)

--- src/Controller/VacancyController.php
+++ src/Controller/VacancyController.php
@@ -6,13 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Service\VacancyService;
+use App\Service\CompanyService;
+use App\Service\EmployerService;
+
 class VacancyController extends AbstractController
 {
-    #[Route('/vacancy', name: 'app_vacancy')]
-    public function index(): Response
+
+    private $vacancyService;
+    private $candidateService;
+    private $companyService;
+    private $employerService;
+
+    public function __construct(VacancyService $vacancyService, CompanyService $companyService, EmployerService $employerService) {
+        $this->vacancyService = $vacancyService;
+        $this->employerService = $employerService;
+        $this->companyService = $companyService;
+    }
+
+    #[Route('/vacancy/{id}', name: 'app_vacancy')]
+    public function vacancy($id): Response
     {
+        $vacancy = $this->vacancyService->fetchVacancy($id);
+        $relatedVacancies = $this->vacancyService->fetchRelatedVacancies($id);
+
         return $this->render('vacancy/index.html.twig', [
             'controller_name' => 'VacancyController',
+            'vacancy' => $vacancy,
+            'related_vacancies' => $relatedVacancies
         ]);
     }
 }
