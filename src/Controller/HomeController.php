d4b20b8d1ce6871c4c8446d483b02ee61397574f
Jelle Z. <public@jellezwie.rs>
2023-11-20 09:12:46 +0100

    Add basic homepage controller (#15)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Service\CandidateService;
+use App\Service\VacancyService;
+
 class HomeController extends AbstractController
 {
+    private $vacancyService;
+    private $candidateService;
+
+    public function __construct(VacancyService $vacancyService, CandidateService $candidateService) {
+        $this->vacancyService = $vacancyService;
+        $this->candidateService = $candidateService;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $recentVacancies = $this->vacancyService->fetchRecentVacancies(3);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'vacancies' => $recentVacancies
         ]);
     }
 }
