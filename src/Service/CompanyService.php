b78417929b2ddad29b920a9105a7a3fc29fdbda8
Jelle Z. <public@jellezwie.rs>
2023-11-20 09:07:48 +0100

    Add company service

---
+++ src/Service/CompanyService.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Candidate;
+use App\Entity\Company;
+use App\Entity\Employer;
+use App\Entity\Submission;
+use App\Entity\Vacancy;
+
+use App\Repository\CandidateRepository;
+use App\Repository\CompanyRepository;
+use App\Repository\EmployerRepository;
+use App\Repository\SubmissionRepository;
+use App\Repository\VacancyRepository;
+
+class CompanyService
+{
+
+    /** @var CandidateRepository $candidateRepository */
+    private $candidateRepository;
+    /** @var CompanyRepository $companyRepository */
+    private $companyRepository;
+    /** @var EmployerRepository $employerRepository */
+    private $employerRepository;
+    /** @var SubmissionRepository $submissionRepository */
+    private $submissionRepository;
+    /** @var VacancyRepository $vacancyRepository */
+    private $vacancyRepository;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->candidateRepository = $em->getRepository(Candidate::class);
+        $this->companyRepository = $em->getRepository(Company::class);
+        $this->employerRepository = $em->getRepository(Employer::class);
+        $this->submissionRepository = $em->getRepository(Submission::class);
+        $this->vacancyRepository = $em->getRepository(Vacancy::class);
+    }
+
+    public function fetchCompany($id = null)
+    {
+        if (is_null($id)) return (null);
+        return ($this->companyRepository->find($id));
+    }
+
+    // private function fetchPoppodium($id) {
+    //     return($this->poppodiumRepository->fetchPoppodium($id));
+    // }
+
+    // public function saveOptreden($params) {
+    //     $data = [
+    //       "id" => (isset($params["id"]) && $params["id"] != "") ? $params["id"] : null,
+    //       "omschrijving" => $params["omschrijving"],
+    //       "datum" => new \DateTime($params["datum"]),
+    //       "prijs" => $params["prijs"],
+    //       "ticket_url" => $params["ticket_url"],
+    //       "afbeelding_url" => $params["afbeelding_url"],              
+    //       "poppodium" => $this->fetchPoppodium($params["poppodium_id"]),
+    //       "voorprogramma" => $this->fetchArtiest($params["voorprogramma_id"]),
+    //       "hoofdprogramma" => $this->fetchArtiest($params["hoofdprogramma_id"])
+    //     ];
+
+    //     $result = $this->optredenRepository->saveOptreden($data);
+    //     return($result);
+    // }
+}
